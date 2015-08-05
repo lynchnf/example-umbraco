2467bfcb1e0c76eeff1ef67d926f2bf921668307
Norman Lynch <nlynch@psclistens.com>
2015-08-05 12:38:31 -0500

    Add contact us form, 404 page, and add alt to images.

---
+++ WebApplication/Controllers/ContactUsSurfaceController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    public class ContactUsSurfaceController : SurfaceController
+    {
+        public ActionResult Index()
+        {
+            return PartialView("ContactUsForm", new ContactUsModel());
+        }
+
+        [HttpPost]
+        public ActionResult HandleFormSubmit(ContactUsModel model)
+        {
+            if (!ModelState.IsValid)
+                return CurrentUmbracoPage();
+
+            MailMessage mailMessage = new MailMessage();
+            string fromEmail = CurrentPage.GetProperty("fromEmail").Value.ToString();
+            mailMessage.From = new System.Net.Mail.MailAddress(fromEmail);
+
+            int toDepartmentId = model.ToDepartmentId;
+            //TODO Replace this with a database query.
+            string toEmail = "default@example.com";
+            if (toDepartmentId == 1)
+            {
+                toEmail = "one@example.com";
+            }
+            else if (toDepartmentId == 2)
+            {
+                toEmail = "two@example.com";
+            }
+            else if (toDepartmentId == 3)
+            {
+                toEmail = "three@example.com";
+            }
+            mailMessage.To.Add(toEmail);
+
+            string subject = model.Subject;
+            string body = "-------------------------------------------------------------------------------\r\n";
+            body += "    Message sent from " + model.ReplyToEmail + " on " + DateTime.Now.ToShortDateString() + ".\r\n";
+            body += "-------------------------------------------------------------------------------\r\n";
+            if (model.Message == null || model.Message == "")
+            {
+                subject += "<eom>";
+            }
+            else
+            {
+                body += model.Message;
+            }
+            mailMessage.Subject = subject;
+            mailMessage.Body = body;
+
+            SmtpClient smtpClient = new SmtpClient();
+            smtpClient.Send(mailMessage);
+
+            var successMessage = CurrentPage.GetProperty("successMessage").Value.ToString();
+            TempData["contactUsSuccess"] = successMessage;
+
+            return RedirectToCurrentUmbracoPage();
+        }
+    }
+}
---
+++ WebApplication/Models/ContactUsModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication.Models
+{
+    public class ContactUsModel
+    {
+        [Display(Name = "From Email")]
+        [Required]
+        [EmailAddress]
+        public string ReplyToEmail { get; set; }
+
+        [Display(Name = "To Department")]
+        [Required]
+        public int ToDepartmentId { get; set; }
+
+        [Display(Name = "Subject")]
+        [Required]
+        public string Subject { get; set; }
+
+        [Display(Name = "Message")]
+        public string Message { get; set; }
+    }
+}
